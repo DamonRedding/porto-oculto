cc030908b41b7f0a14b127bd643355a44124c940
fmmricardo <ricardofilipe5@sapo.pt>
2018-06-15 11:57:29 +0100

    Create the first component - Button

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Porto Oculto',
   },
+<<<<<<< HEAD
   plugins: ['gatsby-plugin-react-helmet']
+=======
+  plugins: ['gatsby-plugin-react-helmet'],
+>>>>>>> 37571cd... Create the first component - Button
 }
