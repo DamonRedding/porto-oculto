18fa8bc05d698e5c006493a6fc74a122ad9f41b8
fmmricardo <ricardofilipe5@sapo.pt>
2018-06-15 12:02:33 +0100

    deleted the plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Porto Oculto',
   },
-<<<<<<< HEAD
+
   plugins: ['gatsby-plugin-react-helmet']
-=======
-  plugins: ['gatsby-plugin-react-helmet'],
->>>>>>> 37571cd... Create the first component - Button
+
 }
