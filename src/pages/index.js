3f53bfc051a004fc5e6e7e2d4ef55716429b357d
fmmricardo <ricardofilipe5@sapo.pt>
2018-06-16 21:33:15 +0100

    adding a button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, {Components} from 'react';
+
+import Link from 'gatsby-link';
+import Button  from '../components/Button';
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <Button>View Other Places</Button>
   </div>
 )
 
