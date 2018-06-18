a7ab5b402a5bd197e0b618febe4b3ec8d2694977
fmmricardo <ricardofilipe5@sapo.pt>
2018-06-18 12:25:41 +0100

    ready for revision

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React, {component} from 'react'
 import Link from 'gatsby-link'
 import ImageFeed from '../components/ImageFeed'
 
+
+
 const IndexPage = () => (
   <div>
     <ImageFeed src="https://placeimg.com/327/172/any" alt="Alternate Text">This is a Title</ImageFeed>
