b7860752abde8197ca144f7af6f60a9b19d5c86d
fmmricardo <ricardofilipe5@sapo.pt>
2018-06-18 14:10:37 +0100

    Camera icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React, {component} from 'react'
 import Link from 'gatsby-link'
-import ImageFeed from '../components/ImageFeed'
-
+import IconCamera from '../components/IconCamera'
 
 
 const IndexPage = () => (
-  <div>
-    <ImageFeed src="https://placeimg.com/327/172/any" alt="Alternate Text">This is a Title</ImageFeed>
-  </div>
+    <IconCamera/>
 )
 
 export default IndexPage
