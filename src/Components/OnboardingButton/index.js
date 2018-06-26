a830d822b254f400f9498d049572d50d580de3d8
fmmricardo <ricardofilipe5@sapo.pt>
2018-06-26 12:11:53 +0100

    adding onboarding button update

--- src/Components/OnboardingButton/index.js
+++ src/Components/OnboardingButton/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import './index.css';
-import './shape.svg'
+import arrow from './shape.svg'
 
 
 export default class OnboardingButton extends Component {
@@ -9,7 +9,7 @@ export default class OnboardingButton extends Component {
   render(){
     return(
       <button className="OnboardingButton">{this.props.children}</button>
-      <img atl="Arrow" src={Shape}>
+        <img alt="TNDS logo" src={ arrow } className="CloseIcon"/>
     );
   }
 
