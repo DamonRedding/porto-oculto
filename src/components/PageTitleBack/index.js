13504d00dc4a229ddfeb6d4a3f0b34e58301b2a5
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-27 15:08:36 +0100

    eslinter erros correction

--- src/components/PageTitleBack/index.js
+++ src/components/PageTitleBack/index.js
@@ -1,16 +1,20 @@
-import React, { Component }  from 'react'
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 import BackArrow from './back.svg';
-import './index.css'
+import './index.css';
 
 
-export default class PageTitleBack extends Component{
-  render(){
-    return(
+export default class PageTitleBack extends Component {
+  render() {
+    return (
       <div className="PageTitleIcons">
-        <img alt="TNDS logo" src={ BackArrow } className="BackArrow"/>
+        <img alt="TNDS logo" src={BackArrow} className="BackArrow" />
         <h1>{this.props.children}</h1>
       </div>
     );
   }
 }
+PageTitleBack.propTypes = {
+  children: PropTypes.string.isRequired,
+};
