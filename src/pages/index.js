8fc7cdcc82b2f65d7e948aebc63b32221b5ef578
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-27 12:03:14 +0100

    eslinter error solved

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 
-import React, {component} from 'react'
-import Link from 'gatsby-link'
-import IconCamera from '../components/IconCamera'
+import React from 'react';
+import IconCamera from '../components/IconCamera';
 import Button from '../components/Button';
 
 
 const IndexPage = () => (
   <div>
     <IconCamera />
-   <Button>View Other Places</Button>
+    <Button>View Other Places</Button>
   </div>
 )
 
