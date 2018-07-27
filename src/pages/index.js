3aec39feda1d7ddc774dfd0f42502834482d564b
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-27 15:35:39 +0100

    correctinng erros

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@
 import React from 'react';
 
 import Button from '../components/Button';
+import IconCamera from '../components/IconCamera';
 
 const IndexPage = () => (
   <div>
+    <IconCamera />
     <Button>View Other Places</Button>
   </div>
 );
